0c9f21282c5e9d3e271b135872cd6129e81e5304
zhengzimin <zhengzimin@laiqutech.com>
2019-09-26 15:59:51 +0800

    合并至指定分支 并推送远端

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source ~/Desktop/udbbbn/git-shell/util.sh
+
+branch=$1
+
+branchVerify $branch
+
+statusCheck
+
+curBranch=$(git rev-parse --abbrev-ref HEAD)
+echo "当前分支为 ${curBranch}"
+
+echo "切换至分支 ${branch}"
+git checkout $branch
+
+echo "正在变基"
+git rebase $curBranch
+
+echo "正在推送远端 ${branch}"
+git push origin $branch
+
+echo "切换至分支 ${curBranch}"
+git checkout $curBranch
+
+echo "脚本执行完成"
